cb097c5317dbabe9ae28a52e62f3f57d543b82cc
VladimirShaybalov <vladimirleonidovich0806@gmail.com>
2024-11-08 10:28:46 +0300

    Add files via upload

---
+++ veriables.py
@@ -0,0 +1,4 @@
+completed_homework = int(12)            #Количество выполненных ДЗ
+hours_spent = float(1.5)                #Количество затраченных часов
+course_name = str('Python')             #Название курса
+print ('Курс:' , course_name , 'всего задач:' , completed_homework , ',' , 'затрачено часов:' , hours_spent , ',' 'среднее время выполнения:' , hours_spent/completed_homework , 'часа.')  #Вывод на экран(в консоль), используя переменные.
