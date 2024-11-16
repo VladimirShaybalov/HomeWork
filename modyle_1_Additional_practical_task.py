fd1c9eba4657ca21c99af27438303c41db8a7984
VladimirShaybalov <vladimirleonidovich0806@gmail.com>
2024-11-16 18:34:05 +0300

    Update modyle_1_Additional_practical_task.py

--- modyle_1_Additional_practical_task.py
+++ modyle_1_Additional_practical_task.py
@@ -1,24 +1,22 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
-
 aeron_=grades[0]
 bilbo_=grades[1]
-Johnny_=grades[2]
+johnny_=grades[2]
 khendric_=grades[3]
-Steve_=grades[4]
+steve_=grades[4]
 aeron_=sum(aeron_)/len(aeron_)
 bilbo_=sum(bilbo_)/len(bilbo_)
 khendric_=sum(khendric_)/len(khendric_)
-Johnny_=sum(Johnny_)/len(Johnny_)
-Steve_=sum(Steve_)/len(Steve_)
-students_ = student={}
-students_.update({'Aeron':aeron_,
-                'Bilbo':bilbo_,
-                'Johnny':Johnny_,
-                'Steve':Steve_})
-
-print(students_)
+johnny_=sum(johnny_)/len(johnny_)
+steve_=sum(steve_)/len(steve_)
+students_=students
+list_students_ = list(students_)
+list_students_.sort()
+list_grades_=[aeron_,bilbo_,johnny_,khendric_,steve_]
+list_grades_list_students_ = {obj : state for obj,state in zip(list_students_,list_grades_)}
+print(list_grades_list_students_)
 
 
 
