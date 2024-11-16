4fa6ead5b58e4848c0dfa77557cc4650e6049e24
VladimirShaybalov <vladimirleonidovich0806@gmail.com>
2024-11-16 13:38:37 +0300

    Add files via upload

---
+++ modyle_1_Additional_practical_task.py
@@ -0,0 +1,30 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+
+aeron_=grades[0]
+bilbo_=grades[1]
+Johnny_=grades[2]
+khendric_=grades[3]
+Steve_=grades[4]
+aeron_=sum(aeron_)/len(aeron_)
+bilbo_=sum(bilbo_)/len(bilbo_)
+khendric_=sum(khendric_)/len(khendric_)
+Johnny_=sum(Johnny_)/len(Johnny_)
+Steve_=sum(Steve_)/len(Steve_)
+students_ = student={}
+students_.update({'Aeron':aeron_,
+                'Bilbo':bilbo_,
+                'Johnny':Johnny_,
+                'Steve':Steve_})
+
+print(students_)
+
+
+
+
+
+
+
+
+
