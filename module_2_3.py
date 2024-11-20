07fc0649c1dd19e5879e12fb75a7a8feb37297b8
VladimirShaybalov <vladimirleonidovich0806@gmail.com>
2024-11-20 11:28:56 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,30 @@
+my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+zero_ = 0
+
+while zero_ < len(my_list) :
+    if my_list[zero_] == 0 :
+        zero_ += 1
+        continue
+    print (my_list[zero_])
+    zero_ += 1
+    if my_list[zero_] < 0 :
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
