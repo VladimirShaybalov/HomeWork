7164041794c9da21d90552059de18210901559cd
VladimirShaybalov <vladimirleonidovich0806@gmail.com>
2024-12-06 15:59:38 +0300

    Add files via upload

---
+++ module_4/fake_math.py
@@ -0,0 +1,9 @@
+
+
+def divide(first, second):
+
+    if second != 0:
+        answer_ = first / second
+        return answer_
+    else:
+        return 'Ошибка'
---
+++ module_4/true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+
+    if second != 0:
+        answer_ = first / second
+        return answer_
+    else:
+            return inf
